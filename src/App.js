49e5ca191b8836ff6b2110345ee1f343cfaf6fbe
Daniel Frimpong <danielfrempong25@gmail.com>
2023-03-23 15:11:05 +0000

    react-basic-jsx

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        FAVOURITE FOOD TO EAT
+      </h1>
+      <p className='food'>
+        Pasta and ceasar sallads are my favourite thing to eat
+        i love when my pasta is creamy and has prawns in them 
+        with abit of spice.we all know why is neccessary to eat 
+        your greens on a daily.i jus dey love sallads!!
+      </p>
+      <h2>SEAFOOD PASTA</h2>
+      <img src='https://www.thespruceeats.com/thmb/P0eVIoQSdFwtJcEmAQIi5iQYjps=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pasta-with-seafood-mushroom-cream-sauce-3060673-step-07-e640a08582be43e69236265ca8c60117.jpg' alt=''></img>
+      <h2>CEASAR SALLAD</h2>
+      <img src='https://cupfulofkale.com/wp-content/uploads/2022/05/Vegan-Chicken-Caesar-Salad-720x720.jpg' alt=''></img>
     </div>
   );
 }
